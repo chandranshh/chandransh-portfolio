aa55ba6682c2ac1943fa9e6cfa7a38366565b260
chandranshh <chandranshyay@gmail.com>
2023-06-06 17:14:13 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="bg-[#1a1a1a] h-screen">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+  return (
+    <div className="bg-gray-200 w-full h-20 flex justify-between px-4 items-center">
+      <div className="text-black">Get in touch</div>
+      <div className="text-black">About</div>
+    </div>
+  );
+}
+
+export default Navbar;
