d201cf77e47ce5c71085020b9ad5f98970fcd06b
chandranshh <chandranshyay@gmail.com>
2023-06-08 15:59:43 +0530

    made some change in the navbar (still broken)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,34 @@
+import {
+  faLinkedinIn,
+  faGithub,
+  faCode,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function Navbar() {
   return (
-    <div className="bg-gray-200 w-full h-20 flex justify-between px-4 items-center">
-      <div className="text-black">Get in touch</div>
-      <div className="text-black">About</div>
+    <div className="w-full h-20 flex justify-between px-4 items-center font-manrope font-light">
+      <div className="text-[#7d7d7d] cursor-pointer">Get in touch</div>
+      <div>
+        <div className="sm:hidden md:hidden lg:flex lg:gap-4 text-[#7d7d7d]">
+          <div className="cursor-pointer sm:hidden md:hidden ">LinkedIn</div>
+          <div>/</div>
+          <div className="cursor-pointer sm:hidden md:hidden ">GitHub</div>
+          <div>/</div>
+          <div className="cursor-pointer sm:hidden md:hidden ">LeetCode</div>
+        </div>
+        <div className="lg:hidden sm:flex sm:gap-2 md:flex md:gap-2 text-[#7d7d7d]">
+          <div>
+            <FontAwesomeIcon icon={faLinkedinIn} size="xs" />
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faGithub} size="xs" />
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faCode} size="xs" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
